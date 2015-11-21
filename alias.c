7cc94276e76fe7933bdcc8ea35bf6a7a02937b85
Loïc Damien <loic.damien@dzamlo.ch>
2015-11-21 22:39:06 +0100

    Add more alias function

--- alias.c
+++ alias.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
 #include "alias.h"
+
+alias_t new_alias_table() {
+  return NULL;
+}
+
+void free_alias_table(alias_t *alias_table) {
+  
+}
+
+int register_alias(alias_t *alias_table, char *alias_name, char *alias_value) {
+  return 0;
+}
+
+char *get_alias_value(alias_t alias_table, char *alias_name) {
+  return NULL;
+}
--- alias.h
+++ alias.h
@@ -1,12 +1,16 @@
 #ifndef ALIAS_H
 #define ALIAS_H
 
-typedef struct alias_st {
+struct alias_st {
   char *alias_name;
   char *alias_value;
   struct alias_st *next;
-} alias_t;
+};
 
+typedef struct alias_st *alias_t;
+
+alias_t new_alias_table();
+void free_alias_table(alias_t *alias_table);
 int register_alias(alias_t *alias_table, char *alias_name, char *alias_value);
 char *get_alias_value(alias_t alias_table, char *alias_name);
 
