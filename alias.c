0a68bcd9c6c805519abd4eee36eba244d49d9f8e
Loïc Damien <loic.damien@dzamlo.ch>
2015-11-21 23:09:59 +0100

    Make format

--- alias.c
+++ alias.c
@@ -5,9 +5,7 @@ alias_t new_alias_table() {
   return NULL;
 }
 
-void free_alias_table(alias_t *alias_table) {
-  
-}
+void free_alias_table(alias_t *alias_table) {}
 
 int register_alias(alias_t *alias_table, char *alias_name, char *alias_value) {
   return 0;
