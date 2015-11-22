ad591b5d88a31f483175942241abeb2e3c40052e
Loïc Damien <loic.damien@dzamlo.ch>
2015-11-22 23:39:47 +0100

    Add newline add the end of perror_fd output

--- errors.c
+++ errors.c
@@ -8,6 +8,6 @@
 void perror_fd(int fd, const char *s) {
   if (fd != -1) {
     char *error_msg = strerror(errno);
-    dprintf(fd, "%s: %s", s, error_msg);
+    dprintf(fd, "%s: %s\n", s, error_msg);
   }
 }
