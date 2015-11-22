48c65a24442317b03fa360c8aaad7ff3b1a87552
Loïc Damien <loic.damien@dzamlo.ch>
2015-11-22 17:20:17 +0100

    Add some test for builtins

--- tests.c
+++ tests.c
@@ -1,4 +1,5 @@
 #include "tests_alias.c"
+#include "tests_builtins.c"
 
 /* Add definitions that need to be in the test runner's main file. */
 GREATEST_MAIN_DEFS();
@@ -9,5 +10,6 @@ GREATEST_MAIN_DEFS();
 int main(int argc, char **argv) {
   GREATEST_MAIN_BEGIN(); /* init & parse command-line args */
   RUN_SUITE(alias);
+  RUN_SUITE(builtins);
   GREATEST_MAIN_END(); /* display results */
 }
---
+++ tests_builtins.c
@@ -0,0 +1,65 @@
+#include "greatest.h"
+#include "builtins.h"
+
+TEST builtin_cd_permission_denied(void) {
+  char *argv[] = {"cd", "/root"};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  char *old_path = getcwd(NULL, 0);
+  int cd_exit_status = builtin_cd(-1, -1, argc, argv);
+  char *new_path = getcwd(NULL, 0);
+  ASSERT_EQ(1, cd_exit_status);
+  ASSERT_STR_EQ(old_path, new_path);
+  PASS();
+}
+
+TEST builtin_cd_no_such_directory(void) {
+  char *argv[] = {"cd", "non_existing_direcotry"};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  char *old_path = getcwd(NULL, 0);
+  int cd_exit_status = builtin_cd(-1, -1, argc, argv);
+  char *new_path = getcwd(NULL, 0);
+  ASSERT_EQ(1, cd_exit_status);
+  ASSERT_STR_EQ(old_path, new_path);
+  PASS();
+}
+
+TEST builtin_cd_dot(void) {
+  char *argv[] = {"cd", "."};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  char *old_path = getcwd(NULL, 0);
+  int cd_exit_status = builtin_cd(-1, -1, argc, argv);
+  char *new_path = getcwd(NULL, 0);
+  ASSERT_EQ(0, cd_exit_status);
+  ASSERT_STR_EQ(old_path, new_path);
+  PASS();
+}
+
+TEST builtin_cd_existing_directory(void) {
+  char *argv[] = {"cd", ".git"};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  char *old_path = getcwd(NULL, 0);
+  int cd_exit_status = builtin_cd(-1, -1, argc, argv);
+  char *new_path = getcwd(NULL, 0);
+  ASSERT_EQ(0, cd_exit_status);
+  ASSERT(strcmp(old_path, new_path) < 0);
+  PASS();
+}
+
+TEST builtin_cd_dotdot(void) {
+  char *argv[] = {"cd", ".."};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  char *old_path = getcwd(NULL, 0);
+  int cd_exit_status = builtin_cd(-1, -1, argc, argv);
+  char *new_path = getcwd(NULL, 0);
+  ASSERT_EQ(0, cd_exit_status);
+  ASSERT(strcmp(old_path, new_path) > 0);
+  PASS();
+}
+
+SUITE(builtins) {
+  RUN_TEST(builtin_cd_permission_denied);
+  RUN_TEST(builtin_cd_no_such_directory);
+  RUN_TEST(builtin_cd_dot);
+  RUN_TEST(builtin_cd_existing_directory);
+  RUN_TEST(builtin_cd_dotdot);
+}
